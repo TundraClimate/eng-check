d771eb5ce9be9f00f319b2aad149705bdc53ce97
TundraClimate <tundra.tundere@gmail.com>
2023-04-21 22:50:49 +0900

    feat: setup files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::error::Error;
+use std::fs;
+use std::fs::File;
+
+fn setup_files() -> Result<(), Box<dyn Error>> {
+    if let Some(s) = dirs::config_dir() {
+        let s = s.join("eng-check");
+        fs::create_dir_all(&s)?;
+        File::create(s.join(".token"))?;
+        File::create(s.join("eng-check.conf"))?;
+    }
+    Ok(())
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn Error>> {
+    setup_files()?;
+    Ok(())
 }
