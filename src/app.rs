af0d778cfc0225ac2b826628918ee908455ce4dd
TundraClimate <tundra.tundere@gmail.com>
2023-05-13 22:33:41 +0900

    feat: render responce-ui

--- src/app.rs
+++ src/app.rs
@@ -4,20 +4,27 @@ use crossterm::{
     event::{self, Event, KeyCode},
 };
 
-use tui::{
-    backend::Backend,
-    Terminal
-};
+use tui::{backend::Backend, Frame, Terminal};
+use tui::layout::Rect;
 
-use crate::ui;
+use crate::{ui};
 
 pub struct App {
     openai_token: String,
     buffer: String,
+    response: Vec<String>,
     should_exit: bool,
 }
 
 impl App {
+    pub fn buffer(&self) -> &String {
+        &self.buffer
+    }
+    
+    pub fn response(&self) -> &Vec<String> {
+        &self.response
+    }
+
     fn exit(&mut self) {
         self.should_exit = true
     }
@@ -28,23 +35,30 @@ impl App {
                 KeyCode::Esc => self.exit(),
                 KeyCode::Char(c) => self.buffer.push(c),
                 KeyCode::Backspace => { self.buffer.pop(); },
+                KeyCode::Enter => {
+                },
                 _ => {}
             }
         }
         Ok(())
     }
 
+    fn render<B: Backend>(&self, f: &mut Frame<B>, area: Rect) {
+        ui::main_ui(&self, f, area);
+    }
+
     pub fn new(openai_token: String) -> App {
         App {
             openai_token,
             buffer: String::new(),
+            response: vec![],
             should_exit: false,
         }
     }
 
     pub fn run<B: Backend>(mut self, terminal: &mut Terminal<B>) -> Result<(), Box<dyn Error>> {
         loop {
-            terminal.draw(|f| ui::main_ui(f, f.size()))?;
+            terminal.draw(|f| self.render(f, f.size()))?;
             App::on_key(&mut self)?;
 
             if self.should_exit { return Ok(()) }
--- src/ui.rs
+++ src/ui.rs
@@ -1,7 +1,9 @@
 use tui::backend::Backend;
 use tui::Frame;
 use tui::layout::{Alignment, Constraint, Direction, Layout, Rect};
+use tui::text::{Span, Spans};
 use tui::widgets::{Block, Borders, Paragraph};
+use crate::app::App;
 
 pub fn loading<B: Backend>(f: &mut Frame<B>, area: Rect) {
     let text = Paragraph::new("now loading").alignment(Alignment::Center);
@@ -18,8 +20,31 @@ pub fn loading<B: Backend>(f: &mut Frame<B>, area: Rect) {
     f.render_widget(text, area);
 }
 
-pub fn main_ui<B: Backend>(f: &mut Frame<B>, area: Rect) {
+pub fn main_ui<B: Backend>(app: &App, f: &mut Frame<B>, area: Rect) {
     let outline = Block::default().borders(Borders::ALL);
+    let layout = Layout::default().direction(Direction::Vertical)
+        .constraints([
+            Constraint::Length(3),
+            Constraint::Length(1),
+            Constraint::Min(1),
+        ].as_ref()).margin(1).split(area);
 
     f.render_widget(outline, area);
+    input_ui(app, f, layout[0]);
+    response_ui(app, f, layout[2]);
+}
+
+fn input_ui<B: Backend>(app: &App, f: &mut Frame<B>, area: Rect) {
+    let outline = Block::default().title("text").borders(Borders::ALL);
+    let text = Paragraph::new(Span::raw(app.buffer())).block(outline);
+
+    f.render_widget(text, area);
+}
+
+fn response_ui<B: Backend>(app: &App, f: &mut Frame<B>, area: Rect) {
+    let outline = Block::default().title("response").borders(Borders::ALL);
+    let spans = Spans(app.response().iter().map(|v| Span::raw(v)).collect::<Vec<Span>>());
+    let text = Paragraph::new(spans).block(outline);
+
+    f.render_widget(text, area);
 }
